53e1a6bddf755351241adbc8b010e6be44fa87fa
박현재 <37414117+ParkHyeonJae@users.noreply.github.com>
2021-04-03 23:29:13 +0900

    Add files via upload

---
+++ MovePath.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node
+{
+    public int index;
+    public Vector3 position;
+
+    public Node(int index, Vector3 position)
+    {
+        this.index = index;
+        this.position = position;
+    }
+}
+
+public class MovePath : MonoBehaviour
+{
+    public List<List<Node>> movePaths = new List<List<Node>>()
+    {
+        new List<Node>() { new Node(0, Vector3.zero)  },
+    };
+
+
+    public bool ShowNodeButtons = true;
+    public bool ShowNodeHandles = true;
+
+    public int destination { get; set; } = 1;
+    public int start { get; set; } = 0;
+    public List<Node> points = new List<Node>();
+
+    public void StartBFSSearch(System.Action<List<Node>> action)
+    {
+        BFS(start);
+
+        foreach (var v in points)
+        {
+            Debug.Log(v.index + "-> ");
+        }
+
+        action?.Invoke(points);
+    }
+
+    public void AddVertex(Vector3 position)
+    {
+        movePaths.Add(new List<Node>() { new Node(movePaths.Count, position) });
+    }
+
+    public void AttachEdge(Node vertex01,Node vertex02)
+    {
+        if (vertex01.index == vertex02.index)
+            return;
+        Debug.Log("Attached");
+        movePaths[vertex01.index].Add(vertex02);
+        movePaths[vertex02.index].Add(vertex01);
+
+        Debug.Log("=================[vertex01]================");
+        Debug.Log($"[vertex01] 추가된 항목 : {vertex01.index}");
+        for (int i = 0; i < movePaths[vertex01.index].Count; i++)
+            Debug.Log($"[vertex01] 전체 항목 : {movePaths[vertex01.index][i].index}");
+        Debug.Log("=================[vertex01]================");
+
+        Debug.Log("=================[vertex02]================");
+        Debug.Log($"[vertex02] 추가된 항목 : {vertex02.index}");
+        for (int i = 0; i < movePaths[vertex02.index].Count; i++)
+            Debug.Log($"[vertex02] 전체 항목 : {movePaths[vertex02.index][i].index}");
+        Debug.Log("=================[vertex02]================");
+    }
+
+    List<List<Node>> GetVertexs() => movePaths;
+
+    [ContextMenu("ResetPaths")]
+    public void ResetPaths()
+    {
+        movePaths = new List<List<Node>>()
+        {
+            new List<Node>() { new Node(0, Vector3.zero)  },
+        };
+        points.Clear();
+    }
+
+    [ContextMenu("BFS")]
+    public void PrintBFSResult()
+    {
+        destination = 2;
+
+        BFS(0);
+
+        foreach (var v in points)
+        {
+            Debug.Log(v.index  + "-> ");
+        }
+    }
+
+    public void PrintNodeByIndex(int index)
+    {
+        Debug.Log($"{index} node Info");
+        foreach (var v in movePaths[index])
+        {
+            Debug.Log($"{v.index} : {v.position}");
+        }
+    }
+
+    
+    public void BFS(int start)
+    {
+        Queue<int> q = new Queue<int>();
+        bool[] visited = new bool[movePaths.Count + 1];
+        int[] parent = new int[movePaths.Count + 1];
+        points.Clear();
+
+        q.Enqueue(start);
+        visited[start] = true;
+        parent[start] = movePaths[start][0].index;
+        Debug.Log("start : " + movePaths[start][0].index);
+
+        while (q.Count > 0)
+        {
+            int now = q.Dequeue();
+            Debug.Log("now: " + now);
+            Debug.Log("movePaths[now].Count: " + movePaths[now].Count);
+            for (int i = 1; i < movePaths[now].Count; i++)
+            {
+                Debug.Log($"Process{i} : " + movePaths[now][i].index);
+                if (visited[movePaths[now][i].index])
+                    continue;
+                Debug.Log($"Enqueue{i} : " + movePaths[now][i].index);
+                q.Enqueue(movePaths[now][i].index);
+                visited[movePaths[now][i].index] = true;
+                parent[movePaths[now][i].index] = now;
+            }
+        }
+
+        Debug.Log("dest : " + destination);
+
+        for (int i = 0; i < parent.Length; i++)
+        {
+            Debug.Log(i + " : " + parent[i]);
+        }
+
+        int dest = destination;
+        points.Add(movePaths[destination][0]);
+        while (parent[dest] != dest)
+        {
+            Debug.Log("parent[destination] : " + parent[dest]);
+            points.Add(movePaths[parent[dest]][0]);
+
+            dest = parent[dest];
+        }
+        Debug.Log("end Index : " + movePaths[destination][0].index);
+        
+        points.Reverse();
+    }
+}
---
+++ MovePath_Editor.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+
+[CustomEditor(typeof(MovePath))]
+public class MovePath_Editor : Editor
+{
+    MovePath movePath;
+
+    GUIStyle stringStyle = new GUIStyle();
+    GUIStyle stringStyle2 = new GUIStyle();
+    GUIStyle stringStyle3 = new GUIStyle();
+
+    float buttonXOffset = 5.0f;
+
+    private void OnEnable()
+    {
+        movePath = (MovePath)target;
+
+        stringStyle.normal.textColor = Color.green;
+        stringStyle.fontStyle = FontStyle.Bold;
+        stringStyle.fontSize = 30;
+        stringStyle.alignment = TextAnchor.MiddleCenter;
+
+        stringStyle2.normal.textColor = Color.red;
+        stringStyle2.fontStyle = FontStyle.Bold;
+        stringStyle2.fontSize = 10;
+        stringStyle2.alignment = TextAnchor.MiddleCenter;
+
+
+        stringStyle3.fontSize = 10;
+        stringStyle3.alignment = TextAnchor.MiddleCenter;
+        attachCnt = 0;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        DrawDefaultInspector();
+    }
+
+    Node[] selectedNode = new Node[2];
+    int attachCnt = 0;
+
+    int nodeInfo = 0;
+
+    private void OnSceneGUI()
+    {
+        movePath = (MovePath)target;
+
+        var buttonStyle = new GUIStyle(GUI.skin.button);
+        buttonStyle.normal.textColor = Color.white;
+        buttonStyle.fontStyle = FontStyle.Bold;
+        buttonStyle.fontSize = 15;
+        buttonStyle.alignment = TextAnchor.MiddleCenter;
+
+        Vector3[] graphVertexPosition = new Vector3[movePath.movePaths.Count + 1];
+        for (int i = 0; i < movePath.movePaths.Count; i++)
+        {
+            if (movePath.ShowNodeHandles)
+            {
+                buttonXOffset = Handles.ScaleValueHandle(buttonXOffset, movePath.movePaths[i][0].position, Quaternion.identity, buttonXOffset, Handles.ArrowHandleCap, 0.5f);
+                movePath.movePaths[i][0].position = Handles.PositionHandle(movePath.movePaths[i][0].position, Quaternion.identity);
+            }
+            Vector3 position = movePath.movePaths[i][0].position;
+            Handles.Label(position, i.ToString(), stringStyle);
+
+            Vector3[] graphEdgePosition = new Vector3[movePath.movePaths[i].Count + 1];
+
+            if (movePath.ShowNodeButtons)
+            {
+                Handles.BeginGUI();
+
+                GUI.backgroundColor = new Color(0.3f, 0.5f, 2);
+                Rect buttonRect = new Rect(HandleUtility.WorldToGUIPoint(position).x + buttonXOffset, HandleUtility.WorldToGUIPoint(position).y - 65, 100, 20);
+                if (GUI.Button(buttonRect, "Attach", buttonStyle))
+                {
+                    selectedNode[attachCnt] = new Node(i, position);
+                    attachCnt++;
+
+                    if (attachCnt == 2)
+                    {
+                        movePath.AttachEdge(selectedNode[0], selectedNode[1]);
+                        attachCnt = 0;
+                    }
+                }
+
+                GUI.backgroundColor = Color.red;
+                Rect buttonRect2 = new Rect(HandleUtility.WorldToGUIPoint(position).x + buttonXOffset, HandleUtility.WorldToGUIPoint(position).y - 95, 100, 20);
+                if (GUI.Button(buttonRect2, "AddVertex", buttonStyle))
+                {
+                    movePath.AddVertex(position);
+                }
+                Handles.EndGUI();
+            }
+
+            //Handles.color = new Color(2, 0.5f, 0.2f, 5);
+            Handles.color = Color.white;
+            for (int j = 1; j < movePath.movePaths[i].Count; j++)
+            {
+                Handles.DrawAAPolyLine(10, movePath.movePaths[i][0].position, movePath.movePaths[i][j].position);
+                //Handles.DrawLine(movePath.movePaths[i][0].position, movePath.movePaths[i][j].position);
+            }
+        }
+
+        for (int i = 0; i < movePath.points.Count - 1; i++)
+        {
+            Handles.color = new Color(2, 0.5f, 0.2f, 5);
+            Handles.DrawAAPolyLine(10, movePath.points[i].position, movePath.points[i + 1].position);
+        }
+
+        GUI.backgroundColor = Color.black;
+        Handles.BeginGUI();
+
+        GUILayout.BeginVertical();
+
+        GUILayout.BeginArea(new Rect(20, 20, 60, 500));
+
+        //GUILayout.Label("Test");
+
+        EditorGUILayout.LabelField("StartNode", stringStyle2);
+        movePath.start = EditorGUILayout.IntField(movePath.start, GUILayout.MinHeight(60));
+
+        EditorGUILayout.LabelField("DestNode", stringStyle2);
+        movePath.destination = EditorGUILayout.IntField(movePath.destination, GUILayout.MinHeight(60));
+
+        if (GUILayout.Button("Search", GUILayout.MinHeight(50)))
+        {
+
+            movePath.StartBFSSearch(null);
+        }
+        
+        EditorGUILayout.LabelField("PrintNode", stringStyle2);
+        nodeInfo = EditorGUILayout.IntField(nodeInfo, GUILayout.MinHeight(60));
+        if (GUILayout.Button("NodeInfo", GUILayout.MinHeight(50)))
+        {
+            movePath.PrintNodeByIndex(nodeInfo);
+        }
+
+        GUILayout.EndArea();
+
+        GUILayout.EndVertical();
+
+        Handles.EndGUI();
+
+    }
+
+    
+}
